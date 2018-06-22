967353bf1763c192fd38cf838f22a3a983a2a143
hartly97 <mhartwikitree@gmail.com>
2018-06-22 08:28:29 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+
+import React, { Component } from 'react';
+import './App.css';
+import * as firebase from 'firebase';
+import RoomsList from './components/RoomsList';
+
+
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBUzguapbzMPREIitzLUqfwISGrv9lncq4",
+    authDomain: "bloc-chat-react-84eab.firebaseapp.com",
+    databaseURL: "https://bloc-chat-react-84eab.firebaseio.com",
+    projectId: "bloc-chat-react-84eab",
+    storageBucket: "bloc-chat-react-84eab.appspot.com",
+    messagingSenderId: "198907327366"
+};
+firebase.initializeApp(config);
+
+
+
+class App extends Component {
+    render () {
+        return (
+            <div className='App'>
+                <header>
+                    <h1> BLOC CHAT! </h1>
+                </header>
+                <main>
+                    <section id="sidebar">
+                        <RoomsList firebase={firebase} />
+                    </section>
+                    <section id="main">
+                        <p>This side will contain main info</p>
+                    </section>
+                </main>
+            </div>
+        )
+    }
+}
+
+export default App;
