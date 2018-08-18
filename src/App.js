d21edaf887c7bc10e2a0e645d405ff12bf966e85
hartly97 <mhartwikitree@gmail.com>
2018-08-18 10:07:56 -0500

    views

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import RoomList from './components/RoomList';
 import MessageList from './components/MessageList';
 import User from './components/User';
 
-// Initialize Firebase
+
 var config = {
     apiKey: "AIzaSyBUzguapbzMPREIitzLUqfwISGrv9lncq4",
     authDomain: "bloc-chat-react-84eab.firebaseapp.com",
@@ -16,45 +16,54 @@ var config = {
 };
 firebase.initializeApp(config);
 
-
 class App extends Component {
     constructor(props) {
-        super(props);
-
-        this.state={
-            activeRoom: ''
-        }
-
-        this.changeActiveRoom = this.changeActiveRoom.bind(this);
+      super(props)
+      this.state = {
+        user: '',
+        activeRoom: '',
+        activeMessage: ''
+      }
+      this.setActiveRoom = this.setActiveRoom.bind(this);
+      this.setMessage = this.setMessage.bind(this);
+      this.setUser = this.setUser.bind(this);
     }
 
-    changeActiveRoom(room) {
-        this.setState({ activeRoom: room })
-        console.log(this.state.activeRoom)
+    setMessage(message) {
+     this.setState({ activeMessage: message })
     }
 
-
-    render () {
-        return (
-            <div className='App'>
-                <main>
-                    <section id="sidebar">
-                        <RoomList
-                            firebase={firebase}
-                            activeRoom={this.state.activeRoom}
-                            changeActiveRoom={this.changeActiveRoom}
-                        />
-                    </section>
-                    <section id="main">
-                        <MessageList
-                            firebase={firebase}
-                            activeRoom={this.state.activeRoom}
-                        />
-                    </section>
-                </main>
-            </div>
-        )
+    setUser(user) {
+      this.setState({ user: user })
     }
+
+    setActiveRoom(room) {
+      this.setState({ activeRoom: room })
+    }
+
+  render() {
+    return (
+      <div className="app container-fluid">
+        <h1 className="chat-header">Bloc Chat</h1>
+         <User
+          firebase={ firebase }
+          user={this.state.user}
+          setUser={(user) => this.setUser(user)}
+          />
+        <RoomList
+          firebase={ firebase }
+          setActiveRoom={ this.setActiveRoom }
+          activeRoom={ this.state.activeRoom}
+          />
+        <MessageList
+          firebase={ firebase }
+          activeRoom={this.state.activeRoom}
+          setActiveRoom={this.setActiveRoom}
+          user={ this.state.user}
+          />
+      </div>
+    );
+  }
 }
 
 export default App;
